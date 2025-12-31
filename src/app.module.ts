e2f8b8627e6edbe521c512c4d08186e343313929
Alex <alexander.tamayo@globant.com>
2025-12-31 10:25:54 -0500

    feat(app): add CharacterService to providers in AppModule
    - Import CharacterService from characters module
    - Add it to the providers array to enable dependency injection
    - This allows the service to be injected into controllers or other services within the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
+import { CharacterService } from './characters/characters.service';
 
 @Module({
   imports: [CharactersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CharacterService],
 })
 export class AppModule {}
