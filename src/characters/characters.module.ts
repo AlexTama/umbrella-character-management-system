17ded8777a3bdd3fe1d7ef1559f0c1ac1091131c
Alex <alexander.tamayo@globant.com>
2025-12-31 10:26:13 -0500

    feat(characters): add CharacterService provider to CharactersModule
    - Import CharacterService in characters.module.ts
    - Add CharacterService to providers array in @Module decorator
    - Enables dependency injection for CharacterService within the CharactersModule scope in NestJS application

--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CharactersController } from './characters.controller';
+import { CharacterService } from './characters.service';
 
 @Module({
   controllers: [CharactersController],
+  providers: [CharacterService],
 })
 export class CharactersModule {}
